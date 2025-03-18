07cc84076d282f112e4c58ec0440524101d1da24
Lendie Follett <53866498+LendieFollett@users.noreply.github.com>
2025-03-18 13:52:43 -0500

    add rmse non-ratio plots

--- src/results_simulated_data.R
+++ src/results_simulated_data.R
@@ -35,11 +35,17 @@ p <- rmse_ratio_plot(results_combined, k = "Not Sparse", sig = 15)+ labs(caption
 ggsave(paste0("RMSE_ratios_",FALSE,"_15",".pdf"),plot = p, width = 20, height = 15)
 
 #LRF TO DO: MAKE PROBIT A DASHED LINE
-#is this one necessary? it's just the first plot, but not relative to probit
-p <- rmse_plot(results_combined,k="Sparse", sig= 7) + ggtitle("Sparse");p
-ggsave(paste0("RMSE_", TRUE, ".pdf"), width = 20, height = 15)
-p <- rmse_plot(results_combined,k="Not Sparse", sig = 15)+ ggtitle("Not Sparse");p
-ggsave(paste0("RMSE_", FALSE, ".pdf"), width = 20, height = 15)
+p <- rmse_plot(results_combined, k = "Sparse", sig = 7) + labs(caption="Sigma = 7, sparse coefficient vector");p
+ggsave(paste0("RMSE_",TRUE,"_7",".pdf"),plot = p, width = 20, height = 15)
+
+p <- rmse_plot(results_combined, k = "Sparse", sig = 15) + labs(caption="Sigma = 15, sparse coefficient vector");p
+ggsave(paste0("RMSE_",TRUE,"_15",".pdf"),plot = p, width = 20, height = 15)
+
+p <- rmse_plot(results_combined, k = "Not Sparse", sig = 7)+ labs(caption="Sigma = 7, full coefficient vector");p
+ggsave(paste0("RMSE_",FALSE,"_7",".pdf"),plot = p, width = 20, height = 15)
+
+p <- rmse_plot(results_combined, k = "Not Sparse", sig = 15)+ labs(caption="Sigma = 15, full coefficient vector");p
+ggsave(paste0("RMSE_",FALSE,"_15",".pdf"),plot = p, width = 20, height = 15)
 
 
 ##PRECISION - machine learning algorithms can be less stable
