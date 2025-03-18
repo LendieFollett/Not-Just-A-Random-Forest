512e2337ac37f3a8b89b4647d759a6d3adad97be
Lendie Follett <53866498+LendieFollett@users.noreply.github.com>
2025-03-18 12:59:46 -0500

    rename plots

--- src/results_simulated_data.R
+++ src/results_simulated_data.R
@@ -22,16 +22,16 @@ results_combined <- results_combined %>%
 
 
 # COMPARING RMSE - need to also look at bias
-p <- rmse_ratio_plot(results_combined, k = "Sparse", sig = 7) + ggtitle("Sparse");p
+p <- rmse_ratio_plot(results_combined, k = "Sparse", sig = 7) + labs(caption="Sigma = 7, sparse coefficient vector");p
 ggsave(paste0("RMSE_ratios_",TRUE,"_7",".pdf"),plot = p, width = 20, height = 15)
 
-p <- rmse_ratio_plot(results_combined, k = "Sparse", sig = 15) + ggtitle("Sparse");p
+p <- rmse_ratio_plot(results_combined, k = "Sparse", sig = 15) + labs(caption="Sigma = 15, sparse coefficient vector");p
 ggsave(paste0("RMSE_ratios_",TRUE,"_15",".pdf"),plot = p, width = 20, height = 15)
 
-p <- rmse_ratio_plot(results_combined, k = "Not Sparse", sig = 7)+ ggtitle("Not Sparse");p
+p <- rmse_ratio_plot(results_combined, k = "Not Sparse", sig = 7)+ labs(caption="Sigma = 7, full coefficient vector");p
 ggsave(paste0("RMSE_ratios_",FALSE,"_7",".pdf"),plot = p, width = 20, height = 15)
 
-p <- rmse_ratio_plot(results_combined, k = "Not Sparse", sig = 15)+ ggtitle("Not Sparse");p
+p <- rmse_ratio_plot(results_combined, k = "Not Sparse", sig = 15)+ labs(caption="Sigma = 15, full coefficient vector");p
 ggsave(paste0("RMSE_ratios_",FALSE,"_15",".pdf"),plot = p, width = 20, height = 15)
 
 #LRF TO DO: MAKE PROBIT A DASHED LINE
